99612905e50a4b3c99eb3c1ec486e7336dd9a416
Foster Taylor <taylorf2@aetna.com>
2021-07-18 09:24:23 -0600

    Adds function to refute text displayed and updates function name for readability

--- app/src/androidTest/ReusableFunctions/ReusableFunctions.kts
+++ app/src/androidTest/ReusableFunctions/ReusableFunctions.kts
@@ -1,9 +1,9 @@
 
-fun assertViewIdIsDisplayed(view: ViewInteraction){
+fun assertViewIsDisplayed(view: ViewInteraction){
     view.check(matches(isDisplayed()));
 }
 
-fun assertViewTextIsDisplayed(text: String){
+fun assertTextIsDisplayed(text: String){
     onView(withText(text)).check(matches(isDisplayed()));
 }
 
@@ -12,3 +12,5 @@ fun refuteViewIsEnabled(view: ViewInteraction) = view.check(matches(not(isEnable
 fun assertViewIsEnabled(view: ViewInteraction) = view.check(matches(isEnabled()))
 
 fun refuteViewIsDisplayed(view: ViewInteraction) = view.check(doesNotExist())
+
+fun refuteTextIsDisplayed(text: String) = view.check(doesNotExist())
