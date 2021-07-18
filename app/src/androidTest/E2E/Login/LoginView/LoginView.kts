8256a960043d98d0d219d821d461a45f6adbd9c5
Foster Taylor <taylorf2@aetna.com>
2021-07-18 09:07:22 -0600

    Adds function to assert for both email and password error alerts during login

--- app/src/androidTest/E2E/Login/LoginView/LoginView.kts
+++ app/src/androidTest/E2E/Login/LoginView/LoginView.kts
@@ -16,3 +16,39 @@ fun assertLoginViewDisplay(){
     assertViewIdIsDisplayed(loginButton)
     assertViewIdIsDisplayed(signupLink)
 }
+
+fun assertEmailAndPasswordInputErrorAlerts() {
+//    Initial view
+    refuteViewIsDisplayed(emailInputError)
+    refuteViewIsDisplayed(paswordInputError)
+    refuteViewIsEnabled(loginButton)
+    //   Error alert for empty input
+    emailInput.perform(replaceText(""))
+    passwordInput.perform(replaceText(""))
+    refuteViewIsEnabled(loginButton)
+    assertViewIsDisplayed(emailInputError)
+    assertViewIsDisplayed(paswordInputError)
+    //    Error alert for incorrect email format
+    emailInput.perform(replaceText("apple"))
+    assertViewIsDisplayed(emailInputError)
+    //    Error alert for less than 4 password characters
+    passwordInput.perform(replaceText("app"))
+    assertViewIsDisplayed(paswordInputError)
+    refuteViewIsEnabled(loginButton)
+    //    Error alert for more than 19 characters
+    passwordInput.perform(replaceText("abcdefghijklmnopqrst"))
+    assertViewIsDisplayed(paswordInputError)
+    refuteViewIsEnabled(loginButton)
+    //    Error alert not displayed for correct email and password format
+    emailInput.perform(replaceText("apple@icloud.com"))
+    //    assert login button not enabled until both inputs filled in correctly
+    refuteViewIsEnabled(loginButton)
+    passwordInput.perform(replaceText("apple"))
+    emailInput.perform(replaceText("incorrect"))
+    refuteViewIsEnabled(loginButton)
+    //    Both inputs now filled in correctly and login button should be enabled and error alerts not displayed
+    emailInput.perform(replaceText("apple@icloud.com"))
+    assertViewIsEnabled(loginButton)
+    refuteViewIsDisplayed(emailInputError)
+    refuteViewIsDisplayed(paswordInputError)
+}
