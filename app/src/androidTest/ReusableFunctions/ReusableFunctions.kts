859c45cfa33d2cd913707ac64515852c58b69320
Foster Taylor <taylorf2@aetna.com>
2021-07-18 09:06:26 -0600

    Adds more reusable functions

--- app/src/androidTest/ReusableFunctions/ReusableFunctions.kts
+++ app/src/androidTest/ReusableFunctions/ReusableFunctions.kts
@@ -6,3 +6,9 @@ fun assertViewIdIsDisplayed(view: ViewInteraction){
 fun assertViewTextIsDisplayed(text: String){
     onView(withText(text)).check(matches(isDisplayed()));
 }
+
+fun refuteViewIsEnabled(view: ViewInteraction) = view.check(matches(not(isEnabled())))
+
+fun assertViewIsEnabled(view: ViewInteraction) = view.check(matches(isEnabled()))
+
+fun refuteViewIsDisplayed(view: ViewInteraction) = view.check(doesNotExist())
