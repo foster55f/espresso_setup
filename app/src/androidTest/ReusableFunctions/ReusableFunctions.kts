825cfea2b1e338ad27ed8e2308af9a1a766ecebb
Foster Taylor <taylorf2@aetna.com>
2021-07-18 09:31:51 -0600

    Moves function in reusable functions file to group with other common assertions for readability

--- app/src/androidTest/ReusableFunctions/ReusableFunctions.kts
+++ app/src/androidTest/ReusableFunctions/ReusableFunctions.kts
@@ -6,9 +6,9 @@ fun assertTextIsDisplayed(text: String) {
     onView(withText(text)).check(matches(isDisplayed()));
 }
 
-fun refuteViewIsEnabled(view: ViewInteraction) = view.check(matches(not(isEnabled())))
-
 fun assertViewIsEnabled(view: ViewInteraction) = view.check(matches(isEnabled()))
+
+fun refuteViewIsEnabled(view: ViewInteraction) = view.check(matches(not(isEnabled())))
 
 fun refuteViewIsDisplayed(view: ViewInteraction) = view.check(doesNotExist())
 
