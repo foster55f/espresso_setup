876499e8b302ed6595fb541158b4cf9899821f26
Foster Taylor <taylorf2@aetna.com>
2021-07-18 09:08:49 -0600

    Adds error and password error alert function to verify login view test function

--- app/src/androidTest/E2E/Login/Login.kts
+++ app/src/androidTest/E2E/Login/Login.kts
@@ -1,11 +1,18 @@
+import org.junit.Rule
+import org.junit.Test
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
 class LoginTestSuite  {
 
     @get: Rule
     val activityScenarioRule =
-            ActivityScenarioRule(com.sourcey.materiallogindemo.MainActivity::class.java)
+            ActivityScenarioRule(com.sourcey.materiallogindemo.LoginAcitivty::class.java)
 
+    @test
     fun verifyLoginView() {
         assertLoginViewDisplay()
+        assertEmailAndPasswordInputErrorAlerts()
     }
-
 }
